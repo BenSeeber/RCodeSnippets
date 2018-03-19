f29fe06e561afe809cdc6c7ff69d379214413f37
Benjamin Seeber <Benjamin.Seeber@five1.de>
2018-03-19 13:10:24 +0100

    Add nonlinear version

--- plot_svm.R
+++ plot_svm.R
@@ -1,4 +1,4 @@
-#Plot SVM Results for Linear Kernel
+# Plot SVM Results for Linear Kernel
 
 x1s <- c(.3,1,1,1.9,3,3.5,     1,3.5,4,5,5.5,4)
 x2s <- c(3.5,1,2.5,2,1,1.2,  5.8,3,4,5,4,1)
@@ -21,3 +21,14 @@ p <- svm.model$SV
 abline(a=-b/w[1,2], b=-w[1,1]/w[1,2], col="black", lty=1)
 abline(a=(-b-1)/w[1,2], b=-w[1,1]/w[1,2], col="orange", lty=3)
 abline(a=(-b+1)/w[1,2], b=-w[1,1]/w[1,2], col="orange", lty=3)
+
+
+# Nonlin
+dat <- my.data
+fit = svm(type ~ ., data = dat, scale = FALSE, kernel = "radial", cost = 5)
+
+
+xgrid = expand.grid(x1=seq(min(x1s),max(x1s),by=0.05), x2=seq(min(x2s),max(x2s),by=0.05))
+ygrid = predict(fit, xgrid)
+plot(xgrid, col = as.numeric(ygrid), pch = 20, cex = 0.2)
+points(x, col = y + 1, pch = 19)
